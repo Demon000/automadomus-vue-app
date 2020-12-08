ef79266cd0f52879853bfb128cc6478b191319b4
Demon000 <demonsingur@gmail.com>
2020-12-08 19:16:45 +0200

    vue-app: move app to components

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="../assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './HelloWorld.vue'
 
 export default {
   name: 'App',
