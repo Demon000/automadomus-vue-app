e3002c72c921c17be405ac34f25d0916389271a7
Demon000 <demonsingur@gmail.com>
2020-12-10 20:33:53 +0200

    vue-app: fix vue cli, remove vite

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pages: {
+        index: {
+            entry: 'src/js/main.js',
+            template: 'src/index.html',
+            filename: 'index.html',
+        },
+    },
+};
