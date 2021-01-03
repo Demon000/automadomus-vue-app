6fe375da3a8fda2953fb4e0cdfe0f2dd358ca538
Demon000 <demonsingur@gmail.com>
2021-01-03 15:29:52 +0200

    vue-app: emit network errors from API

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,10 +1,19 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { camelKeys, snakeKeys } from 'js-convert-case/lib';
+import { isNetworkError } from '@/utils/misc';
+import EventEmitter from 'eventemitter3';
+
+export enum APIEvents {
+    NETWORK_ERROR = 'network-error',
+}
 
 export default class API {
     public baseRequest: AxiosInstance;
+    public emitter: EventEmitter;
 
     constructor(baseURL: string) {
+        this.emitter = new EventEmitter();
+
         this.baseRequest = axios.create({
             baseURL,
             withCredentials: true,
@@ -32,6 +41,10 @@ export default class API {
                 return response;
             },
             (error: Error) => {
+                if (isNetworkError(error)) {
+                    this.emitter.emit(APIEvents.NETWORK_ERROR, error);
+                }
+
                 return Promise.reject(error);
             },
         );
